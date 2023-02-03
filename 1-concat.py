f1ed3e35abcb5e7b9c6f3077189bb199054d1aca
Solomon Eniola Adegoke <souceking@gmail.com>
2023-02-03 04:30:30 +0000

    Basic annotations - concat

---
+++ 1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This file contain the concat function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """This function takes in two strings and returns a concatenated string"""
+    return str1 + str2
